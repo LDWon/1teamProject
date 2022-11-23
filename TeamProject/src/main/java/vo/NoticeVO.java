05ca3d4bc2873c4a36a9dcc87f2719ff977b8dca
jaelik <whowhoman@naver.com>
2022-11-23 16:07:43 +0900

    d

--- TeamProject/src/main/java/vo/NoticeVO.java
+++ TeamProject/src/main/java/vo/NoticeVO.java
@@ -1,5 +1,54 @@
 package vo;
 
 public class NoticeVO {
+	
+	private String name,subject,content,pwd,regidate,filename;
+	private int idx;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getRegidate() {
+		return regidate;
+	}
+	public void setRegidate(String regidate) {
+		this.regidate = regidate;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	
 
 }
